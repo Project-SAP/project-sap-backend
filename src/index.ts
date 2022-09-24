93191770c246d02635c5de794d50f06d4b09fd54
Kyle Lastimosa <61811721+glitchkyle@users.noreply.github.com>
2022-09-24 12:16:13 -0400

    fix manual typing of variables

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 
@@ -7,28 +7,28 @@ import cors from "cors";
 /// Might need to use a package such as CORS to address any cross-origin request issues.
 ///
 dotenv.config();
-const app = express();
+const app: Express = express();
 const port = process.env.SERVER_PORT;
 
 app.use(cors());
 
 // call `curl localhost:8080/test` or simply put URL in broswer.
-app.get("/test", (req, res) => {
+app.get("/test", (req: Request, res: Response) => {
     return res.send("This is a test GET payload");
 });
 
 // call `curl -X POST localhost:8080/test`
-app.post("/test", (req, res) => {
+app.post("/test", (req: Request, res: Response) => {
     return res.send("This is a test POST payload");
 });
 
 // call `curl -X PUT localhost:8080/test`
-app.put("/test", (req, res) => {
+app.put("/test", (req: Request, res: Response) => {
     return res.send("This is a test PUT payload");
 });
 
 // call `curl -X DELETE localhost:8080/test`
-app.delete("/test", (req, res) => {
+app.delete("/test", (req: Request, res: Response) => {
     return res.send("This is a test DELETE payload");
 });
 
