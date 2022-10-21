4a168e349554dacd44f59b635f34daf9cba51a66
Nicholas Reel <nicky.shadow99@gmail.com>
2022-10-21 12:33:00 -0400

    split up schema from model interface

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,42 +1,4 @@
-import mongoose, { Schema } from 'mongoose';
-import bcrypt from 'bcrypt';
-
-export interface User {
+export default interface User {
     email: string,
     password: string,
 }
-
-const UserSchema: Schema = new Schema<User>({
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    password: {
-        type: String,
-        required: true,
-    },
-});
-
-UserSchema.pre('save', async function (next) {
-    const user = this;
-    const hash = await bcrypt.hash(user.password, 24);
-
-    this.password = hash;
-    next();
-});
-
-/**
- * Validate a given password with it the related database password
- * @param password login request password that needs validated
- */
-UserSchema.methods.isValidPassword = async function(password: string) {
-    const user = this;
-    const compare = await bcrypt.compare(password, user.password);
-
-    return compare;
-};
-
-const UserModel = mongoose.model<User>('user', UserSchema);
-
-export default UserModel;
