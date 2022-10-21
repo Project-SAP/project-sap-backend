ae461b4da0200456e889a9f39bdc3d58fc930416
Nicholas Reel <nicky.shadow99@gmail.com>
2022-10-20 21:10:48 -0400

    beginning with failing test cases

--- tests/routes/auth.controller.test.ts
+++ tests/routes/auth.controller.test.ts
@@ -2,6 +2,8 @@ import { Express } from 'express';
 import { serverInit } from '../../src/utils/server.utils';
 import { attachControllers } from '@decorators/express';
 import { AuthController } from '../../src/controller/auth.controller';
+import { LoginModel } from './../../src/models/login.model';
+import request from 'supertest';
 
 describe('AuthorizationController', () => {
     const controllerPath = '/auth';
@@ -12,5 +14,49 @@ describe('AuthorizationController', () => {
 
     it('should configure', () => {
         expect(testContext).toBeTruthy();
+    });
+
+    describe('/login', () => {
+        const loginPath = '/login';
+
+        // Mock request data
+        const testEmail = 'some.email@gmail.com';
+        const testPassword = 'aa00';
+
+        describe.each`
+            email        | password        | expectedStatus
+            ${null}      | ${null}         | ${500}
+            ${testEmail} | ${null}         | ${500}
+            ${null}      | ${testPassword} | ${500}
+            ${testEmail} | ${testPassword} | ${200}
+        `(
+            'when validating login request',
+            ({ email, password, expectedStatus }) => {
+                it(`should return a ${expectedStatus}${
+                    email ? '' : ' without an email'
+                }${password ? '' : ' without a password'}`, async () => {
+                    const loginRequest: LoginModel = { email, password };
+
+                    const response = await request(testContext)
+                        .post(`${controllerPath}${loginPath}`)
+                        .send(loginRequest);
+
+                    expect(response.statusCode).toEqual(expectedStatus);
+                });
+            }
+        );
+
+        it('should return back an authroization token to the client', async () => {
+            const loginRequest: LoginModel = {
+                email: testEmail,
+                password: testPassword,
+            };
+
+            const response = await request(testContext)
+                .post(`${controllerPath}${loginPath}`)
+                .send(loginRequest);
+
+            expect(response.statusCode).toEqual(200);
+        });
     });
 });
