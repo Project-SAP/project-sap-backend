0ff97ba43377b4e4fbc8a7cdc110883585df24ac
Kyle Lastimosa <61811721+glitchkyle@users.noreply.github.com>
2022-11-05 22:46:03 -0400

    Initialize socket server

--- src/serverApplication.ts
+++ src/serverApplication.ts
@@ -5,6 +5,9 @@ import express, { Express } from 'express';
 import { connect } from 'mongoose';
 import passport from 'passport';
 import { AuthController } from './controller/auth.controller';
+import { createServer, Server as HTTPServer } from "http";
+import { ClientToServerEvents, ServerToClientEvents, QueueRequestData, Message } from "./models/events.model";
+import { Server as SocketServer } from 'socket.io';
 
 /**
  * Keeps track of application lifecycle and maintains a testable server context.
@@ -12,7 +15,7 @@ import { AuthController } from './controller/auth.controller';
  * Not all fields or methods should be exposed as the application context. If a field is not marked as `private`, document the reason.
  */
 export class ServerApplication {
-    private readonly expressContext: Express;
+    private readonly applicationServerContext: HTTPServer;
 
     private readonly port: string;
 
@@ -21,13 +24,16 @@ export class ServerApplication {
         dotenv.config({ path: `${__dirname}/../config/.env` });
         this.port = process.env.SERVER_PORT;
 
-        this.expressContext = this.serverInit((app: Express) => {
+        const expressApp: Express = this.serverInit((app: Express) => {
             // Bind controllers to application
             attachControllers(app, [AuthController]);
         });
 
-        this.expressContext.listen(this.port, async () => {
-            // TODO: Replace with mongoDB connection configuration
+        this.applicationServerContext = createServer(expressApp);
+
+        this.socketServerInit(this.applicationServerContext);
+
+        this.applicationServerContext.listen(this.port, async () => {
             // tslint:disable-next-line:no-console
             console.log(`started server at http://localhost:${this.port}`);
         });
@@ -52,6 +58,48 @@ export class ServerApplication {
         this.initPassportSession(app);
 
         return app;
+    }
+
+    /**
+     * Initializes socket server with specified routes and route handlers
+     * @param httpServer HTTP server where socket server will be attached
+     * @returns Socket Server
+     */
+     private socketServerInit(httpServer: HTTPServer): SocketServer {
+        const FRONTEND_URL = process.env.FRONTEND_URL;
+
+        const io = new SocketServer<ClientToServerEvents, ServerToClientEvents>(httpServer, {
+            cors: { origin: FRONTEND_URL }
+        });
+
+        // Setup and configure socket routes and events
+        io.on("connection", (socket) => {
+
+            // tslint:disable-next-line:no-console
+            console.log("Socket Connected");
+
+            socket.on("joinQueue", (data: QueueRequestData) => {
+                // tslint:disable-next-line:no-console
+                console.log(`Socket ${data.name} Joined Queue`);
+            });
+
+            socket.on("leaveQueue", (data: QueueRequestData) => {
+                // tslint:disable-next-line:no-console
+                console.log(`Socket ${data.name} Left Queue`);
+            });
+
+            socket.on("message", (data: Message) => {
+                // tslint:disable-next-line:no-console
+                console.log(`Socket ${data.sender} said ${data.message}`);
+            });
+
+            socket.on("disconnect", () => {
+                // tslint:disable-next-line:no-console
+                console.log("Socket Disconnected");
+            });
+        });
+
+        return io;
     }
 
     private initPassportSession(app: Express) {
