7a3d763ff76bde9272b15b70b43279988f83fa24
grahamstephens <49448955+grahamstephens@users.noreply.github.com>
2022-11-17 14:37:37 -0500

    added signup (#8)
    Co-authored-by: steph308 <steph308@uw.edu>
    Co-authored-by: Nicholas Reel <nicky.shadow99@gmail.com>

--- src/data-access/user.db.ts
+++ src/data-access/user.db.ts
@@ -6,4 +6,22 @@ export class UserRepository extends BaseRepository<User> {
     constructor() {
         super('users', UserSchema);
     }
+
+    /**
+     * Query to find single json object of given type `T`
+     * Handles promise resolve and rejection generically.
+     * @template T
+     * @param {string} email json payload to querys
+     * @returns {Promise<T>} single json entity
+     */
+    public create(doc: any): Promise<any> {
+        return new Promise((resolve, reject) => {
+            this.repositoryModel.create(doc, (error: Error, response: any) => {
+                if (error) {
+                    reject(error);
+                }
+                resolve(response);
+            });
+        });
+    }
 }
