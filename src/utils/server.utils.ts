569442e2186de751e44225873cc72b14b9f78b70
Kyle Lastimosa <61811721+glitchkyle@users.noreply.github.com>
2022-12-04 15:12:50 -0500

    SAP-43 Setup socket server and routes (#10)
    Co-authored-by: Kyle Andrei Lastimosa <kyle@Kyles-MacBook-Air-3.local>

--- src/utils/server.utils.ts
+++ src/utils/server.utils.ts
@@ -6,7 +6,7 @@ import cors from 'cors';
  * @param configure Callback to configure routes. Useful for testing specific routes.
  * @returns application context of type `core.Express`
  */
-export function serverInit(configure: (express: Express) => void): Express {
+export function expressServerInit(configure: (express: Express) => void): Express {
     const app: Express = express();
     app.use(cors());
     app.use(express.json());
